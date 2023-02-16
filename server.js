087eeab2a112406c69fcae49b74fdbf2b3c52f93
larsvv99 <lars.van.veldhuizen@hva.nl>
2023-02-16 22:59:12 +0100

    Aanmaken van html & css (Frontend)
    Een kleine begin van het maken van het structuur van de webapp.

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('Hallo')
+const express = require(`express`);
+const app = express();
+const PORT = process.env.PORT || 1337;
+app.get(`/`, onHome).listen(PORT, console.log(`Running on port: ${PORT}`));
+
+function onHome(req, res){
+	res.send(`Hallo`) 
+}
