722ed78c9884344050d2c6e7d42e886f4d3c5ce6
larsvv99 <lars.van.veldhuizen@hva.nl>
2023-02-22 11:17:00 +0100

    Alle documenten updaten
    Grote update aangezien ik het nu pas goed begrijp
    CSS aangepast
    DS_store in Gitignore
    Routes aangemaakt
    HTML aangepast

--- server.js
+++ server.js
@@ -6,3 +6,17 @@ app.get(`/`, onHome).listen(PORT, console.log(`Running on port: ${PORT}`));
 function onHome(req, res){
 	res.send(`Hallo`) 
 }
+
+
+app.get('/filter', (req, res) => {
+	res.send('Filterpagina')
+})
+
+
+app.get('/overons', (req, res) => {
+	res.send('Overons')
+})
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
