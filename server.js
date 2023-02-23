785a1beed37e9e2baaa93d8dfe8f95bf949c5da8
larsvv99 <lars.van.veldhuizen@hva.nl>
2023-02-23 11:56:44 +0100

    Terug naar vorige stap
    Ik heb geen idee hoe ik een stap terug ga met de terminal
    Ook weet ik niet hoe ik mijn pages kan previewen zonder elke keer te pushen

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-import { handlebars } from 'hbs'
 const express = require(`express`)
 const app = express()
 const PORT = process.env.PORT || 1337
@@ -6,18 +5,14 @@ app.get(`/`, onHome).listen(PORT, console.log(`Running on port: ${PORT}`))
 
 app.use(express.static('static'))
 app.use(express.static('views'))
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 app.set('view engine', 'hbs')
 app.set('view engine', 'html')
 app.engine('html', require('hbs').__express)
-=======
-=======
->>>>>>> parent of ed2d45b (HBS werkend maken 1.1)
+
 
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
->>>>>>> parent of ed2d45b (HBS werkend maken 1.1)
 
 function onHome(req, res){
 	res.render(`index.hbs`) 
