49cdcb2b8eea402e8786bbbb57dad436b478eeef
larsvv99 <lars.van.veldhuizen@hva.nl>
2023-03-07 17:04:48 +0100

    Maken van de pagina's
    Verschillende routes maken
    Verschillende hbs bestanden aanmaken
    Code om hbs werkend te krijgen veranderen
    Data aangemaken in json file
    DS store wil ook maar niet wegblijven

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require(`express`);
 const app = express();
+app.use(express.static(`static`));
 const { engine } = require(`express-handlebars`);
-const PORT = 1337;
+const filterdata = require(`./views/data/filters.json`);
+
+//connectie
+const PORT = 4000;
 app.get(`/`, onHome).listen(PORT, console.log(`Running on port: ${PORT}`));
 
-app.use(express.static(`static`));
-app.engine(`.hbs`, engine({ extname: `.hbs` }));
+//error
+// app.use((req, res, next) => {
+// 	res.status(404).send(`sorry can't find that!`);
+// });
+
+//handlebars 
+app.engine(`.hbs`, engine({
+	defaultLayout: `main`,
+	layoutsDir: (__dirname + `/views/layouts`),
+	partialsDir: (__dirname + `/views/partials`),
+	extname: `.hbs`
+}));
 app.set(`view engine`, `.hbs`);
 app.set(`views`, `./views`);
 
 
 
+//routes
+
 function onHome(req, res) {
-	res.render(`test.hbs`);
+	res.render(`main`, { layout: `index` });
+	console.log(filterdata);
 }
 
 app.get(`/`, (req, res) => {
@@ -20,11 +37,12 @@ app.get(`/`, (req, res) => {
 });
 
 app.get(`/filter`, (req, res) => {
-	res.send(`/styles/style.css`);
+	res.render(`filter`, { layout: `index` });
 });
 
 app.get(`/overons`, (req, res) => {
 	res.send(`Overons`);
 });
+
 
 
