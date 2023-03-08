c836bbef384288fd9547973a50abe73085a93845
shortstoryguy <123173720+shortstoryguy@users.noreply.github.com>
2023-03-08 15:44:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const filterdata = require(`./views/data/filters.json`);
 //connectie
 const PORT = 4000;
 
-//error
-// app.use((req, res, next) => {
-// 	res.status(404).send(`sorry can't find that!`);
-// });
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 //handlebars 
 app.engine(`.hbs`, engine({
@@ -19,8 +17,6 @@ app.engine(`.hbs`, engine({
 	partialsDir: (__dirname + `/views/partials`),
 	extname: `.hbs`
 }));
-app.set(`view engine`, `.hbs`);
-app.set(`views`, `./views`);
 
 
 
@@ -31,17 +27,21 @@ function onHome(req, res) {
 	console.log(filterdata);
 }
 
-app.get(`/`, (req, res) => {
+app.get('/', (req, res) => {
 	res.render(onHome);
 });
 
-app.get(`/filter`, (req, res) => {
+app.get('/filter', (req, res) => {
 	res.render(`filter`, { layout: `index` });
 });
 
-app.get(`/overons`, (req, res) => {
+app.get('/overons', (req, res) => {
 	res.send(`Overons`);
 });
+
+app.get('*', (req, res) => {
+  res.send('Not found');
+}
 
 app.listen(PORT, console.log(`Running on port: ${PORT}`));
 
